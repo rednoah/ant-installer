95d27008e207d999dafcd3e72315b84775dffcf5
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-04 08:00:01 +0700

    Build QNAP repo

--- build-ant-qnap-repo.groovy
+++ build-ant-qnap-repo.groovy
@@ -16,7 +16,7 @@ f.withWriter('UTF-8') { writer ->
 			category('Essentials')
 			type('Developer Tools')
 			icon80('https://raw.githubusercontent.com/rednoah/ant-installer/master/package/qnap/icons/apache-ant_80.gif')
-			description("${properties.title} will help you install ${properties.product} ${properties.version} on your Synology NAS. On install, this package will download the Apache Ant and Apache Ivy binaries and optional libraries. This may take a while.")
+			description("${properties.title} will help you install ${properties.product} ${properties.version} on your QNAP NAS. On install, this package will download the Apache Ant and Apache Ivy binaries and optional libraries. This may take a while.")
 			fwVersion('4.2.1')
 			version(properties.version)
 			['TS-269H', 'TS-NASARM', 'TS-NASX86', 'TS-X28', 'TS-X31', 'TS-X31U', 'TS-X41', 'TS-ARM-X09', 'TS-ARM-X19'].each{ id ->
