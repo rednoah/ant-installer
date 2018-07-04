f43893002ceaeb80b4fdd8458805a259a0b3f552
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-04 07:26:32 +0700

    Build QNAP repo

--- build-ant-qnap-repo.groovy
+++ build-ant-qnap-repo.groovy
@@ -9,25 +9,27 @@ f.withWriter('UTF-8') { writer ->
 	def xml = new MarkupBuilder(writer)
 
 	xml.mkp.xmlDeclaration(version: "1.0", encoding: "utf-8")
-	xml.plugins() {
-		cachechk(now.format('yyyyMMddHHmm'))
-		name(properties.title)
-		internalName(properties.package)
-		category('Essentials')
-		type('Developer Tools')
-		icon80('https://github.com/rednoah/java-installer/raw/master/package/qnap/icons/oracle-java_80.gif')
-		description("${properties.title} will help you install ${properties.product} ${properties.version} on your Synology NAS. On install, this package will download the Apache Ant and Apache Ivy binaries and optional libraries. This may take a while.")
-		fwVersion('4.2.1')
-		fwVersion(properties.version)
-		['TS-269H', 'TS-NASARM', 'TS-NASX86', 'TS-X28', 'TS-X31', 'TS-X31U', 'TS-X41', 'TS-ARM-X09', 'TS-ARM-X19'].each{ id ->
-			platform{
-				platformID(id)
-				location("https://github.com/rednoah/ant-installer/releases/download/${properties.version}/${properties.package}-${properties.version}.qpkg")
+	xml.plugins {
+		item {
+			cachechk(now.format('yyyyMMddHHmm'))
+			name(properties.title)
+			internalName(properties.package)
+			category('Essentials')
+			type('Developer Tools')
+			icon80('https://github.com/rednoah/java-installer/raw/master/package/qnap/icons/oracle-java_80.gif')
+			description("${properties.title} will help you install ${properties.product} ${properties.version} on your Synology NAS. On install, this package will download the Apache Ant and Apache Ivy binaries and optional libraries. This may take a while.")
+			fwVersion('4.2.1')
+			fwVersion(properties.version)
+			['TS-269H', 'TS-NASARM', 'TS-NASX86', 'TS-X28', 'TS-X31', 'TS-X31U', 'TS-X41', 'TS-ARM-X09', 'TS-ARM-X19'].each{ id ->
+				platform{
+					platformID(id)
+					location("https://github.com/rednoah/ant-installer/releases/download/${properties.version}/${properties.package}-${properties.version}.qpkg")
+				}
 			}
+			publishedDate(now.format('yyyy/MM/dd'))
+			maintainer('Apache Software Foundation')
+			forumLink('https://github.com/rednoah/ant-installer')
+			language('English')
 		}
-		publishedDate(now.format('yyyy/MM/dd'))
-		maintainer('Apache Software Foundation')
-		forumLink('https://github.com/rednoah/ant-installer')
-		language('English')
 	}
 }
